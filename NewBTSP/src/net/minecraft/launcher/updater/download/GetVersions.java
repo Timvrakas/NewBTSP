02446fb44c4081061a8c3b6c9b4a0be3bde4d3d3
Timvrakas <tim.vrakas@gmail.com>
2013-07-17 13:51:56 -0500

    Finished Forge JSON Parser need mojang parser too

--- NewBTSP/src/net/minecraft/launcher/updater/download/GetVersions.java
+++ NewBTSP/src/net/minecraft/launcher/updater/download/GetVersions.java
@@ -1,19 +1,11 @@
 package net.minecraft.launcher.updater.download;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -23,15 +15,12 @@ import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
-import net.minecraft.launcher.Http;
-
 public class GetVersions {
 	static Gson gson = new Gson();
 	
-	@SuppressWarnings("unused")
-	public static void main(String[] args) throws IOException{
+	public static Map<String,List<String>> getVersions() throws IOException{
 		
-			Map<String,List<String>> result = null;
+			Map<String,List<String>> result = new HashMap<String, List<String>>();
 			URL url = new URL("http://files.minecraftforge.net/minecraftforge/json2");
 			JsonReader reader = new JsonReader(new InputStreamReader(url.openStream(), "UTF-8"));
 	        JsonParser parser = new JsonParser();
@@ -42,20 +31,16 @@ public class GetVersions {
 	  
 	        Type typeOfHashMap = new TypeToken<Map<String,Map<Integer,Map<String,Object>>>>() { }.getType();
 	        Map<String,Map<Integer,Map<String,String>>> mcvers = gson.fromJson(parser.parse(reader),typeOfHashMap);
-			if(true){
+			
 				for(Entry<String, Map<Integer, Map<String, String>>> mcversEntry : mcvers.entrySet()){
 					Map<Integer, Map<String, String>> mcver = mcvers.get(mcversEntry.getKey());
-					result.put(mcversEntry.getKey(),null);
+					List<String> builds = new ArrayList<String>();
+					
 						for(Entry<Integer, Map<String, String>> entry : mcver.entrySet()){
-							System.out.println(entry.getValue().get("version")+"-"+mcversEntry.getKey());
-							result.get(mcversEntry.getKey()).add(entry.getValue().get("version"));
+							builds.add(entry.getValue().get("version"));
+							}
+						result.put(mcversEntry.getKey(),builds);
+						}
+					return result;
 					}
 				}
-			}
-		
-		return;
-		
-	}
-	
-	
-}
--- NewBTSP/src/start.java
+++ NewBTSP/src/start.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -8,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 import net.btsp.Auth;
 import net.btsp.ResourcesDownload;
+import net.minecraft.launcher.Http;
 import net.minecraft.launcher.process.JavaProcess;
 import net.minecraft.launcher.process.JavaProcessLauncher;
 import net.minecraft.launcher.updater.download.DownloadJob;
@@ -19,30 +21,30 @@ public class start {
 	static Auth auth = new Auth();
 	public static void main(String args[]) throws MalformedURLException, IOException, InterruptedException{
 		//auth.login("ben1234neb", "ben5x62fb");
-		auth.refresh();
+		//auth.refresh();
 		
 		
-		JavaProcessLauncher j = new JavaProcessLauncher(null, args);
-		j.directory(new File("/Users/timv/desktop/test/"));
-		JavaProcess jp = j.start();
+		//JavaProcessLauncher j = new JavaProcessLauncher(null, args);
+		//.directory(new File("/Users/timv/desktop/test/"));
+		//JavaProcess jp = j.start();
 		
 		
 		
 		
 		
-		//Set<Downloadable> files = ResourcesDownload.getResourceFiles(new File("/users/timv/desktop"));
-				//DownloadJob j = new DownloadJob("Name", false, files);
-		//ThreadPoolExecutor t = new ThreadPoolExecutor(8, 8, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
-		//j.startDownloading(t);
-		//for ( ; ; ) {
-			//Thread.sleep(1000);
-			//System.out.println(j.getProgress());
-			//if(j.isComplete()){
-				//System.out.println("done");
-				//lt.shutdown();
-				//break;
-			//}
-		//}
+		Set<Downloadable> files = ResourcesDownload.getResourceFiles(new File("/users/timv/desktop"));
+				DownloadJob j = new DownloadJob("Name", false, files);
+		ThreadPoolExecutor t = new ThreadPoolExecutor(8, 8, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+		j.startDownloading(t);
+		for ( ; ; ) {
+			Thread.sleep(1000);
+			System.out.println(j.getProgress());
+			if(j.isComplete()){
+				System.out.println("done");
+				t.shutdown();
+				break;
+			}
+		}
 		//System.out.println(Http.performGet(new URL("https://s3.amazonaws.com/Minecraft.Download/" + "versions/1.6.2.jar")));
 	}
 	
