89b6e0543f85abe42c11b3d5339b4feacf7a756f
Timvrakas <tim.vrakas@gmail.com>
2013-07-17 23:08:13 -0500

    kk now were really done with forge parse

--- NewBTSP/src/net/minecraft/launcher/updater/download/GetVersions.java
+++ NewBTSP/src/net/minecraft/launcher/updater/download/GetVersions.java
@@ -20,7 +20,8 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 
 public class GetVersions {
-	public class Promos {
+	
+public class Promos {
 	public String build;
 	public Map <String, Map <String, Object>> files;
 		
@@ -29,7 +30,6 @@ public class GetVersions {
 	static Gson gson = new Gson();
 	
 	public static Map<String,List<String>> getVersions(File file) throws IOException{
-		
 			Map<String,List<String>> result = new HashMap<String, List<String>>();
 			InputStream in = new FileInputStream(file);
 			JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
@@ -57,14 +57,11 @@ public class GetVersions {
 						Collections.reverse(builds);
 						result.put(mcversEntry.getKey(),builds);
 						}
-				System.out.println("hi");
 				
 				while(reader.hasNext()){
 		        	if(reader.peek()==JsonToken.NAME){
 		        		String s = reader.nextName();
-		        		System.out.println(s);
 		        		if(s.equalsIgnoreCase("promos")){
-		        			System.out.println(s);
 		        			break;
 		        		}
 		        	}else
@@ -78,14 +75,14 @@ public class GetVersions {
 		        for(Entry<String, Promos> promosEntry : promos.entrySet()){
 					if(promosEntry.getKey().startsWith("recommended")){
 						Map<String, Map<String, Object>> promo = promosEntry.getValue().files;
-						String value = promo.get("src").get("version")+" ("+promosEntry.getKey()+")";
-						System.out.println(value);
-						String version = (String) promo.get("src").get("mcversion");
-						List<String>  list = result.get(version);
-						list.add(0, value);
-						result.put(version, list);
+						String recomendedbuild = promo.get("src").get("version")+" ("+promosEntry.getKey()+")";
+						String mcVersion = (String) promo.get("src").get("mcversion");
+						List<String>  list = result.get(mcVersion);
+						list.add(0, recomendedbuild);
+						result.put(mcVersion, list);
 					}
 		        }
 				return result;		
 				}
-			}
+			
+}
